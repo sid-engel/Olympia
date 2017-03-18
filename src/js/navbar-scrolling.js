452790a1917a9010bab0ed791f8457c118b5ad1f
Sid Engel <info@sidengel.com>
2017-03-18 16:33:57 -0600

    Code Cleanup
    Minor code adjustments, just to make it clean.

--- src/js/navbar-scrolling.js
+++ src/js/navbar-scrolling.js
@@ -1,12 +1,3 @@
-//jQuery to collapse the navbar on scroll
-$(window).scroll(function() {
-    if ($(".navbar").offset().top > 50) {
-        $(".navbar-fixed-top").addClass("top-nav-collapse");
-    } else {
-        $(".navbar-fixed-top").removeClass("top-nav-collapse");
-    }
-});
-
 //jQuery for page scrolling feature - requires jQuery Easing plugin
 $(function() {
     $('a.page-scroll').bind('click', function(event) {
