b3c1c2687f912a3cdd09e07b32c461df8feba766
Sid Engel <info@sidengel.com>
2017-08-07 23:30:40 -0400

    Nav Cleanup
    Just some small fixes to the new nav...

--- src/js/smoothscroll.js
+++ src/js/smoothscroll.js
@@ -1,7 +1,6 @@
 $(document).ready(function(){
 	$('a[href^="#"]').on('click',function (e) {
 	    e.preventDefault();
-
 	    var target = this.hash;
 	    var $target = $(target);
 	    $('html, body').stop().animate({
